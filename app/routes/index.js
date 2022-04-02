f949de8cd4d263e0a00079eb831096b4ddb65754
choclacode <saqib@choclacode.eu.org>
2022-04-02 09:02:27 +0600

    re-commit

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,38 +6,6 @@ const { pub } = require('../middlewares')
 const { err404, errorHandler } = require('../middlewares/error')
 
 const root = Router()
-  .get('', async (req, res) => {
-    const { PrismaClient } = require('@prisma/client')
-    const prisma = new PrismaClient()
-    //await prisma.$connect()
-    try {
-      return res.json({
-        success: true,
-        data: await prisma.sus.findMany()
-      })
-    } finally {
-      //prisma.$disconnect()
-    }
-  })
-  .get('/make', async (req, res) => {
-    const { PrismaClient } = require('@prisma/client')
-    const prisma = new PrismaClient()
-    //await prisma.$connect()
-    try {
-      await prisma.sus.create({
-        data: { type: 'SUS AMOGUS' }
-      })
-      await prisma.sus.create({
-        data: { type: 'AMOGUS SUSSS' }
-      })
-      return res.json({
-        success: true,
-        data: await prisma.sus.findMany()
-      })
-    } finally {
-      //prisma.$disconnect()
-    }
-  })
 
 module.exports = process.env.ROOT == 'same'
   ? Router()
